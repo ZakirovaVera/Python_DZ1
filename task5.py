115013121211f927ad4f149481898459e734c717
ZakirovaVera <zakirovavk@gmail.com>
2022-09-23 21:54:15 +0500

    Решение задачи 5 расстояние между точками

--- task5.py
+++ task5.py
@@ -1,6 +1,14 @@
-# Напишите программу, которая принимает на вход координаты двух точек и 
+# Напишите программу, которая принимает на вход координаты двух точек и
 # находит расстояние между ними в 2D пространстве.
 
 # Пример:
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
+import math
+x_coordinateA = int(input('Введите координату Х: '))
+y_coordinateA = int(input('Введите координату Y: '))
+x_coordinateB = int(input('Введите координату Х: '))
+y_coordinateB = int(input('Введите координату Y: '))
+
+distance_between_points = math.sqrt(pow(x_coordinateB-x_coordinateA, 2) + pow(y_coordinateB-y_coordinateA, 2))
+print(f'А {x_coordinateA,y_coordinateA}; B {x_coordinateB,y_coordinateB} -> {round(distance_between_points,2)}')
